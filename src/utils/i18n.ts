7aa9ee639f01bad5d99c79b22a7e80633f746b98
Nicola Marcacci Rossi <nicola@smartive.ch>
2022-11-22 11:09:29 +0100

    fix: Language

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,11 +1,13 @@
 import { get } from "lodash";
 import chBe from "../../locales/ch-be/common.json";
 import en from "../../locales/en/common.json";
+import it from "../../locales/it/common.json";
 import { useData } from "../components/data";
 
 export const LOCALES: Record<string, Record<string, string>> = {
   ["ch-be"]: chBe,
   en,
+  it,
 };
 
 const t = (locale: keyof typeof LOCALES, path: string) =>
